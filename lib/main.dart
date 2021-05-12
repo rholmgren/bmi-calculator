d0a0b676f75b5687f7623b1be31f17daf1c556ef
Rachelle Holmgren <rachelle.holmgren@gmail.com>
2021-05-12 15:53:51 -0700

    Move InputPage widget into separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,17 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
-void main() {
-  runApp(BMICalculator());
-}
+void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: InputPage(),
-      theme: ThemeData(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("BMI Calculator"),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
